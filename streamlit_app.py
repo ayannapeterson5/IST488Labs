689f82c5f85599d9af90c1f3d960f76376625eda
ayannapeterson5 <aypeters@syr.edu>
2026-02-05 18:59:48 +0000

    Final2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,15 @@
+import streamlit as st
+
+choice = st.sidebar.selectbox(
+    "Choose a lab",
+    ["Lab 1", "Lab 2", "Lab 3"]
+)
+
+if choice == "Lab 1":
+    exec(open("Labs/Lab1.py").read())
+elif choice == "Lab 2":
+    exec(open("Labs/Lab2.py").read())
+elif choice == "Lab 3":
+    exec(open("Labs/Lab3.py").read())
 
 
