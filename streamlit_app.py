d9d86c5de6162dc6970ecec023ce55650c426e7c
ayannapeterson5 <aypeters@syr.edu>
2026-02-12 19:30:09 +0000

    Default to Lab 4 on launch

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,10 @@ import streamlit as st
 
 choice = st.sidebar.selectbox(
     "Choose a lab",
-    ["Lab 1", "Lab 2", "Lab 3", "Lab 4"]
+    ["Lab 1", "Lab 2", "Lab 3", "Lab 4"],
+    index=3
 )
+
 
 if choice == "Lab 1":
     exec(open("Labs/Lab1.py").read())
@@ -14,6 +16,4 @@ elif choice == "Lab 3":
 elif choice == "Lab 4":
     exec(open("Labs/Lab4.py").read())
 
-
-exec(open("Labs/Lab4.py").read())
 
